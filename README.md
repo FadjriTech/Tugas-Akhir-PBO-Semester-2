130fa386acc5ea3388e663e1d55c2fc906ba42d7
FadjriTech <faidfadjri.busdev@gmail.com>
2023-05-21 21:27:49 +0700

    add readme and sql exported;

---
+++ README.md
@@ -0,0 +1,43 @@
+
+# Point of sales - Netbeans
+
+Aplikasi point of sales adalah aplikasi kasir dan stok inventory yang dibangun menggunakan Netbeans IDE dan MySQL Databases. Aplikasi ini sangat sederhana dan dibuat untuk kepentingan tugas akhir perkuliahan Teknologi Informasi Semester 2
+
+
+## Pre Requisite
+
+[Install XAMPP](https://www.apachefriends.org/download.html)\
+[Install openJDK 11](https://www.oracle.com/id/java/technologies/downloads/)\
+[Install Netbeans](https://netbeans.apache.org/download/index.html)
+
+
+## Cara Menjalankanya
+
+```http
+  Start Apache && MYSQL
+```
+
+```http
+  Buka Netbeans
+```
+
+```http
+  Pilih Services > Databases Klik Kanan
+```
+
+```http
+  Pilih New Connection > Klik Add
+```
+
+```http
+  Pilih Folder Libraries > MYSQL > Mysql Connector Jar
+```
+
+```http
+  Buka localhost/phpmyadmin buat database pos
+```
+
+```http
+  import pos.sql
+```
+
---
+++ pos.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: May 21, 2023 at 04:26 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stok`
+--
+
+CREATE TABLE `stok` (
+  `id` int(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `price` int(255) NOT NULL,
+  `quantity` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `stok`
+--
+
+INSERT INTO `stok` (`id`, `item`, `price`, `quantity`) VALUES
+(1, 'Poci Listrik', 50000, 5),
+(2, 'Charger Handphone', 125000, 5),
+(3, 'Laptop', 3000000, 6),
+(4, 'Adaptor', 55000, 99);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(50) NOT NULL,
+  `jenis` enum('penjualan','pembelian') NOT NULL,
+  `barang` varchar(255) NOT NULL,
+  `harga` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','pic','finance') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', 'passwordsaya', 'admin'),
+(2, 'pic', 'passwordsaya', 'pic');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `stok`
+--
+ALTER TABLE `stok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `stok`
+--
+ALTER TABLE `stok`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
